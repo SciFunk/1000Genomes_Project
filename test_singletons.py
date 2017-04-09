ad86da01e1c6c0f43ef5304c70f8d825549df602
SciFunk <scientistfunk@gmail.com>
2017-04-09 15:07:28 -0700

    Update test_singletons.py

--- test_singletons.py
+++ test_singletons.py
@@ -1,6 +1,6 @@
 import gzip
 import sys
-from collections import defaultdict #simpler/more automated than using dict to make a dictionary of lists, uses default key value
+#from collections import defaultdict #simpler/more automated than using dict to make a dictionary of lists, uses default key value
 
 singletonpos = [] #list of positions with singletons to tie to sample name, format [14, 16, 273]
 samplenames = [] #a list of samplenames format ['HG00102', 'HG00103', 'HG00105',...]
@@ -21,6 +21,8 @@ with gzip.open(sys.argv[1]) as data: #instead of data = gzip.open((sys.argv[1]),
           else:
               index2 = spline.index("0|1")
               singletonpos.append(index2)
+  #    index3 = spline.index("1")
+   #   singletonpos.append(index3)
             
 #print samplenames 
 #print singletonpos
@@ -32,9 +34,9 @@ for i in singletonpos:
     singletons[i] += 1
 #print singletons
 
-fp = open('pop_locations.txt', 'r')
+pop_locations = open('pop_locations.txt', 'r')
 sample_info = {}
-for line in fp:
+for line in pop_locations:
   spline = line.split()
   sample_info[spline[0]] = spline[1:] #can use 1:3 if you only want pos 1:3 as values
             # defines key      values
@@ -44,18 +46,42 @@ pairs_dict = dict(zip(samplenames, singletons)) #zip creates a paired list from 
 entriesToRemove = ('#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT')
 for k in entriesToRemove:
     pairs_dict.pop(k, None)
-#print pairs_dict
 
 #pairs_dict looks like {HG00102: 1, HG00103: 3, ...}
 #sample_info looks like {HG00102: ['ACB', 'AFR', 'female'], ...}
 
+new_dict = {}
 for keys in pairs_dict.keys():  # pairs_dict.keys = list of the keys in pairs_dict
-  new_dict = {}
-  entry1 = pairs_dict[keys]
-  entry2 = sample_info[keys]
+  entry1 = pairs_dict[keys] #number of singletons
+  entry2 = sample_info[keys] #['GBR', 'EUR', 'female'] 
   pop = entry2[0]
   try:
       new_dict[pop] += int(entry1)
   except KeyError:
       new_dict[pop] = entry1
 print new_dict
+
+# see http://stackoverflow.com/questions/11026959/writing-a-dict-to-txt-file-and-reading-it-back
+def writeDict(dict, filename, sep):
+    with open(filename, "a") as f:
+        for i in dict.keys():            
+            f.write(i + " " + str(dict[i]) + "\n")
+            
+def readDict(filename, sep):
+    with open(filename, "r") as f:
+        dict = {}
+        for line in f:
+            values = line.split(sep)
+            dict[values[0]] = int(values[1])
+        return(dict)
+ 
+dict2 = readDict("outputSingletons.txt"," ")
+for keys in dict2.keys():
+  dict2[keys] += new_dict[keys]
+writeDict(dict2,"outputSingletons.txt"," ")
+
+
+
+
+
+  
