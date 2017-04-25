452172bc72fb87804acb039217cf7a25d49095ae
SciFunk <scientistfunk@gmail.com>
2017-04-25 10:30:06 -0700

    Create variants_fig3a.py

---
+++ variants_fig3a.py
@@ -0,0 +1,89 @@
+import gzip
+import sys
+import math
+
+pop_locations = open('pop_locations.txt', 'r')
+sample_info = {}
+for line in pop_locations:
+  spline = line.split()
+  sample_info[spline[0]] = spline[1:] #can use 1:3 if you only want pos 1:3 as values
+
+# def pairs_dict():
+#   variants = len(samplenames)*[0]
+#   for i in positions:
+#       variants[i] += 1
+#   pairs_dict = dict(zip(samplenames, variants))
+#   return pairs_dict
+
+pop_totals = {'ESN': 99,'GWD': 113,'LWK': 99,'MSL': 85,'YRI': 108,'ACB': 96,
+              'ASW': 61,'CLM': 94,'MXL': 64,'PEL': 85,'PUR': 104,'CDX': 93,
+              'CHB': 103,'CHS': 105,'JPT': 104,'KHV': 99,'CEU': 99,'GBR': 91,
+              'FIN': 99,'IBS': 107,'TSI': 107,'BEB': 86,'GIH': 103,'ITU': 102,
+              'PJL': 96,'STU': 102}
+
+samplenames = [] #a list of samplenames format ['HG00102', 'HG00103', 'HG00105',...]
+final_dict = {'ESN': 0, 'GWD': 0, 'LWK': 0, 'MSL': 0, 'YRI': 0, 'ACB': 0, 'ASW': 0, 'CLM': 0,
+      'MXL': 0, 'PEL': 0, 'PUR': 0, 'CDX': 0, 'CHB': 0, 'CHS': 0, 'JPT': 0, 'KHV': 0,
+      'CEU': 0, 'GBR': 0, 'FIN': 0, 'IBS': 0, 'TSI': 0, 'BEB': 0, 'GIH': 0, 'ITU': 0,
+      'PJL': 0, 'STU': 0}
+
+with gzip.open(sys.argv[1]) as data: #instead of data = gzip.open((sys.argv[1]),"rb"), this will read file line-by-line
+  for line in data:
+      if line.startswith('##'): #skip all ~250 comment lines
+          continue
+      elif line.startswith('#'): #extract line with sample information
+          samplenames = line.split()
+      else:
+        spline = line.split()
+        x = spline.count("0|1")
+        y = spline.count("1|0")
+        z = spline.count("1|1")
+        positions = []
+        if x + y + 2*z < 13:
+          for i,j in enumerate(spline): #i is the index of the element, j is the element itself
+            if j == '0|1':
+              positions.append(i)
+            if j == '1|0':
+              positions.append(i)
+            if j == '1|1':
+              positions.append(i)
+        variants = len(samplenames)*[0]
+        for i in positions:
+          variants[i] += 1
+        pairs_dict = dict(zip(samplenames, variants))
+        entriesToRemove = ('#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT')
+        for k in entriesToRemove:
+            pairs_dict.pop(k, None)
+        blank_dict = {'ESN': 0, 'GWD': 0, 'LWK': 0, 'MSL': 0, 'YRI': 0, 'ACB': 0, 'ASW': 0, 'CLM': 0,
+              'MXL': 0, 'PEL': 0, 'PUR': 0, 'CDX': 0, 'CHB': 0, 'CHS': 0, 'JPT': 0, 'KHV': 0,
+              'CEU': 0, 'GBR': 0, 'FIN': 0, 'IBS': 0, 'TSI': 0, 'BEB': 0, 'GIH': 0, 'ITU': 0,
+              'PJL': 0, 'STU': 0}
+        for keys in pairs_dict.keys():  # pairs_dict.keys = list of the keys in pairs_dict
+          entry1 = pairs_dict[keys]     #number of variants
+          entry2 = sample_info[keys]    #['GBR', 'EUR', 'female']
+          pop = entry2[0]
+          try:
+              blank_dict[pop] += float(entry1)
+          except KeyError:
+              blank_dict[pop] = entry1
+        percent_dict = {k: float(blank_dict[k])/pop_totals[k] for k in pop_totals.viewkeys() & blank_dict}
+        for key in percent_dict:
+            if percent_dict[key] > 0.049:
+                final_dict[key] += 1
+
+def writeDict(dict, filename, sep):
+    with open(filename, "w") as f:
+        for i in dict.keys():
+            f.write(i + " " + str(dict[i]) + "\n")
+def readDict(filename, sep):
+    with open(filename, "r") as f:
+        dict = {}
+        for line in f:
+            values = line.split(sep)
+            dict[values[0]] = int(values[1])
+        return(dict)
+
+dict2 = readDict("variants_fig3a.txt"," ")
+for keys in dict2.keys():
+  dict2[keys] += final_dict[keys]
+writeDict(dict2,"variants_fig3a.txt"," ")
