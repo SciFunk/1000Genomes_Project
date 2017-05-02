9f1e4112d1b31f1a99d145ff8ae359a4f3c00434
SciFunk <scientistfunk@gmail.com>
2017-05-02 12:26:30 -0700

    Update variants_fig3a.py

--- variants_fig3a.py
+++ variants_fig3a.py
@@ -3,24 +3,21 @@ import sys
 import math
 
 def variants_to_blank_dict(samplenames, variants):
-  pairs_dict = dict(zip(samplenames, variants))
-  entriesToRemove = ('#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT')
-  for k in entriesToRemove:
+    pairs_dict = dict(zip(samplenames, variants))
+    entriesToRemove = ('#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT')
+    for k in entriesToRemove:
       pairs_dict.pop(k, None)
-  blank_dict = {'ESN': 0, 'GWD': 0, 'LWK': 0, 'MSL': 0, 'YRI': 0, 'ACB': 0, 'ASW': 0, 'CLM': 0,
+    blank_dict = {'ESN': 0, 'GWD': 0, 'LWK': 0, 'MSL': 0, 'YRI': 0, 'ACB': 0, 'ASW': 0, 'CLM': 0,
         'MXL': 0, 'PEL': 0, 'PUR': 0, 'CDX': 0, 'CHB': 0, 'CHS': 0, 'JPT': 0, 'KHV': 0,
         'CEU': 0, 'GBR': 0, 'FIN': 0, 'IBS': 0, 'TSI': 0, 'BEB': 0, 'GIH': 0, 'ITU': 0,
         'PJL': 0, 'STU': 0}
-  for keys in pairs_dict.keys():  # pairs_dict.keys = list of the keys in pairs_dict
-    entry1 = pairs_dict[keys]     #number of variants
-    entry2 = sample_info[keys]    #['GBR', 'EUR', 'female']
-    pop = entry2[0]
-    try:
+    for keys in pairs_dict.keys():  # pairs_dict.keys = list of the keys in pairs_dict
+        entry1 = pairs_dict[keys]     #number of variants
+        entry2 = sample_info[keys]    #['GBR', 'EUR', 'female']
+        pop = entry2[0]
         blank_dict[pop] += float(entry1)
-    except KeyError:
-         blank_dict[pop] = entry1
-  return(blank_dict)
-
+    return(blank_dict)
+    
 def writeDict(dict, filename, sep):
     with open(filename, "w") as f:
         for i in dict.keys():
@@ -40,11 +37,11 @@ for line in pop_locations:
   spline = line.split()
   sample_info[spline[0]] = spline[1:] #can use 1:3 if you only want pos 1:3 as values
 
-pop_percents = {'ESN': 5,'GWD': 5,'LWK': 5,'MSL': 4,'YRI': 5,'ACB': 5,
-              'ASW': 3,'CLM': 4,'MXL': 3,'PEL': 4,'PUR': 5,'CDX': 4,
-              'CHB': 5,'CHS': 5,'JPT': 5,'KHV': 5,'CEU': 5,'GBR': 4,
-              'FIN': 5,'IBS': 5,'TSI': 5,'BEB': 4,'GIH': 5,'ITU': 5,
-              'PJL': 5,'STU': 5}
+pop_percents = {'ESN': 10,'GWD': 10,'LWK': 10,'MSL': 8,'YRI': 10,'ACB': 10,
+              'ASW': 6,'CLM': 8,'MXL': 6,'PEL': 8,'PUR': 10,'CDX': 8,
+              'CHB': 10,'CHS': 10,'JPT': 10,'KHV': 10,'CEU': 10,'GBR': 8,
+              'FIN': 10,'IBS': 10,'TSI': 10,'BEB': 8,'GIH': 10,'ITU': 10,
+              'PJL': 10,'STU': 10}
 
 samplenames = [] #a list of samplenames format ['HG00102', 'HG00103', 'HG00105',...]
 final_dict = {'ESN': 0, 'GWD': 0, 'LWK': 0, 'MSL': 0, 'YRI': 0, 'ACB': 0, 'ASW': 0, 'CLM': 0,
